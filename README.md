f89833f5be2d657812e9b3163f59e6cfe0aeda06
imshy <imshi1@protonmail.com>
2024-11-19 12:34:08 -0500

    added logging and ability to start where it finishes

---
+++ README.md
@@ -0,0 +1,18 @@
+Python script that uses the free polygon api to get information about all stocks on all days
+
+Create .env and add the api key after the equals sign (no quotes needed):
+```dotenv
+API_KEY=
+```
+
+Install all python dependencies:
+
+```bash
+pip install dotenv requests logging mylib
+```
+
+Run program using:
+
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import time
 
@@ -5,18 +6,22 @@ import requests
 import os
 from dotenv import load_dotenv
 from datetime import timedelta, date
+import logging
 
+logger = logging.getLogger(__name__)
 load_dotenv()
 api_key = os.getenv('API_KEY')
+days_path = './days'
+logging_path = './logging'
 
 
 def main():
-    current_date = date.today()
+    current_date = get_last_recorded_date()
     new_date = current_date
     while True:
         while new_date.weekday() > 4:
             new_date = new_date - timedelta(days=1)
-        fetch_day(str(new_date))
+        fetch_day(new_date.strftime("%Y-%m-%d"))
         time.sleep(15)
         new_date = new_date - timedelta(days=1)
 
@@ -24,11 +29,48 @@ def main():
 def fetch_day(day: str):
     request_link = f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{day}?adjusted=true&include_otc=true&apiKey={api_key}"
     response = requests.get(request_link)
-    print(f"Request link: {request_link}")
-    print(f"code: {response.status_code}")
-    with open(f"days/{day}.json", "w") as file:
+    log(f"Request link: {request_link}")
+    log(f"code: {response.status_code}")
+    with open(f"{days_path}/{day}.json", "w") as file:
         file.write(json.dumps(response.json(), separators=(',', ':'), indent=None, sort_keys=True))
-    print(f"File saved to days/{day}.json")
+    log(f"File saved to {days_path}/{day}.json")
+
+
+def create_folders():
+    try:
+        os.mkdir(days_path)
+    except FileExistsError:
+        log(f"Directory '{days_path}' already exists, continuing")
+
+    try:
+        os.mkdir(logging_path)
+    except FileExistsError:
+        log(f"Directory '{logging_path}' already exists, continuing")
+
+
+def get_last_recorded_date() -> date:
+    files = os.listdir(days_path)
+    files.sort()
+    log(files)
+    if len(files) == 0:
+        return date.today()
+    last_date = files[0].split(".")[0]
+    log(last_date)
+    return datetime.datetime.fromisoformat(last_date)
+
+
+def setup_logging():
+    logging.basicConfig(filename=f'{logging_path}/{date.today().strftime("%m-%d-%Y")}', level=logging.INFO)
+    logger.info('Started')
+
+
+def log(anything):
+    logger.info(str(anything))
+    print(anything)
+
 
 if __name__ == '__main__':
+    create_folders()
     main()
+    setup_logging()
+    logger.info('Finished')
